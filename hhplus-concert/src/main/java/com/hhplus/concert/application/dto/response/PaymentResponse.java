e43b7713974e1dfff06fdaf85019ba6b7264f8a7
JEONGBEOM KO <beom9807@gmail.com>
2024-10-18 06:09:37 +0900

    [dev] 통합테스트 작성
    ### 테스트 코드 설명
    
    - **유저 대기열 토큰 발급 및 조회 테스트**:
        - 대기열 토큰이 생성되고, 대기열 상태와 순번을 조회하여 확인합니다.
    - **예약 가능 날짜 및 좌석 조회 테스트**:
        - `ConcertScheduleRequest`로 예약 가능한 날짜를 요청하고, 응답에서 날짜가 요청한 값과 일치하는지 확인합니다.
        - `SeatRequest`로 예약 가능한 좌석 정보를 요청하고, 좌석의 상태가 `AVAILABLE`인지 검증합니다.
    - **좌석 예약 요청 테스트**:
        - 좌석을 예약하면 임시 상태로 5분간 배정됩니다. `ReservationResponse`의 상태와 만료 시간 검증을 통해 좌석이 올바르게 배정되었는지 확인합니다.
    - **잔액 충전 및 조회 테스트**:
        - 잔액 충전 요청 후 새로운 잔액을 검증하고, 잔액 조회 요청을 통해 충전 후 잔액이 일치하는지 확인합니다.
    - **결제 처리 테스트**:
        - 결제가 완료되면 `PaymentResponse`를 통해 결제 상태와 결제 시간 등을 확인하고, 결제가 완료된 좌석의 상태가 `OCCUPIED`로 변경되었는지 검증합니다.

--- hhplus-concert/src/main/java/com/hhplus/concert/application/dto/response/PaymentResponse.java
+++ hhplus-concert/src/main/java/com/hhplus/concert/application/dto/response/PaymentResponse.java
@@ -4,21 +4,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class PaymentResponse {
+    private Long paymentId;
     private UUID userId;
     private Long seatId;
-    private Long amount;
+    private Long price;
     private String status;
+    private LocalDateTime createdAt;
 
-    public PaymentResponse(UUID userId, Long seatId, Long amount, String status){
+    public PaymentResponse(Long paymentId, UUID userId, Long seatId, Long price, String status, LocalDateTime createdAt) {
+        this.paymentId = paymentId;
         this.userId = userId;
         this.seatId = seatId;
-        this.amount = amount;
+        this.price = price;
         this.status = status;
+        this.createdAt = createdAt;
     }
 }
