2fe51273d01b38429a825a9b3615a922df64ec55
JEONGBEOM KO <beom9807@gmail.com>
2024-11-05 02:57:36 +0900

    refactor: 키를 인덱스 대신 식(expression)으로 정의
    - keyExpression을 사용하여 잠금 키 생성 시 파라미터 순서와 변수명에 의존하지 않으므로 유지보수성을 높입니다.
    -  SpEL을 사용해 키를 정의하므로 코드에 비즈니스 로직에 대한 특정 도메인 키가 강하게 들어가지 않습니다
    -

--- hhplus-concert/src/main/java/com/hhplus/concert/config/aop/RedisLockAspect.java
+++ hhplus-concert/src/main/java/com/hhplus/concert/config/aop/RedisLockAspect.java
@@ -4,6 +4,7 @@ import com.hhplus.concert.config.aop.annotation.RedisLock;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.expression.ExpressionParser;
@@ -14,10 +15,12 @@ import org.springframework.stereotype.Component;
 import java.util.concurrent.TimeUnit;
 
 @Aspect
+//@Order(1) AopForTransaction 클래스 사용 안 하고 메서드에 @Transactional 처리
 @Component
 public class RedisLockAspect {
     private final RedissonClient redissonClient;
     private  final AopForTransaction aopForTransaction;
+    private final ExpressionParser parser = new SpelExpressionParser();
 
     public RedisLockAspect(RedissonClient redissonClient, AopForTransaction aopForTransaction) {
         this.redissonClient = redissonClient;
@@ -26,8 +29,7 @@ public class RedisLockAspect {
 
     @Around("@annotation(redisLock)")
     public Object aroundRedisLock(ProceedingJoinPoint joinPoint, RedisLock redisLock) throws Throwable{
-        Object keyParam = joinPoint.getArgs()[redisLock.keyParameterIndex()];
-        String key = redisLock.keyPrefix() + keyParam.toString();
+        String key = parseKey(redisLock.keyExpression(), joinPoint);
 
         RLock lock = redissonClient.getLock(key);
 
@@ -45,5 +47,19 @@ public class RedisLockAspect {
             }
         }
 
+    }
+
+    private String parseKey(String keyExpression, ProceedingJoinPoint joinPoint) {
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Object[] args = joinPoint.getArgs();
+
+        // 메서드 파라미터 이름과 값을 SpEL 컨텍스트에 추가
+        for (int i = 0; i < signature.getParameterNames().length; i++) {
+            context.setVariable(signature.getParameterNames()[i], args[i]);
+        }
+
+        // keyExpression을 SpEL로 평가하여 키 생성
+        return parser.parseExpression(keyExpression).getValue(context, String.class);
     }
 }
