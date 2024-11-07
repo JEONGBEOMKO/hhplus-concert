92e41e08a0ce9d842502e0a431ee4ea4b6c02914
JEONGBEOM KO <beom9807@gmail.com>
2024-11-08 03:19:54 +0900

    feat: Redis를 이용한 대기열 위치 조회
    Redis Sorted Set을 사용하여 대기열 관리 및 활성화 처리.

--- hhplus-concert/src/main/java/com/hhplus/concert/application/usecase/QueuePositionUseCase.java
+++ hhplus-concert/src/main/java/com/hhplus/concert/application/usecase/QueuePositionUseCase.java
@@ -3,6 +3,8 @@ package com.hhplus.concert.application.usecase;
 import com.hhplus.concert.application.dto.output.QueueOutput;
 import com.hhplus.concert.domain.queue.Queue;
 import com.hhplus.concert.infrastructure.repository.QueueRepository;
+import org.redisson.api.RScoredSortedSet;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -10,37 +12,24 @@ import java.util.UUID;
 
 @Service
 public class QueuePositionUseCase {
-    private final QueueRepository queueRepository;
-    private final CalculateRemainingTimeUseCase calculateRemainingTimeUseCase;
+    private final RedissonClient redissonClient;
+    private static final String QUEUE_KEY = "userQueue";
 
-    public QueuePositionUseCase(QueueRepository queueRepository, CalculateRemainingTimeUseCase calculateRemainingTimeUseCase) {
-        this.queueRepository = queueRepository;
-        this.calculateRemainingTimeUseCase = calculateRemainingTimeUseCase;
+    public QueuePositionUseCase(RedissonClient redissonClient) {
+        this.redissonClient = redissonClient;
     }
 
+
     // 대기열에서 현재 몇 번째 순서인지 조회하는 메서드
-    public QueueOutput getQueuePosition(String token, UUID userId) {
-        Queue queue = queueRepository.findByTokenAndUserId(token, userId)
-                .orElseThrow(() -> new NoSuchElementException("유효하지 않은 토큰입니다."));
+    public long getQueuePosition(String token) {
+        RScoredSortedSet<String> queueSet = redissonClient.getScoredSortedSet(QUEUE_KEY);
+        Integer rank = queueSet.rank(token);
 
-        // 현재 활성화된 대기열 수 확인
-        long activeCount = queueRepository.countByStatus("ACTIVE");
+        if (rank == null) {
+            throw new NoSuchElementException("유효하지 않은 토큰입니다.");
+        }
 
-        // 현재 사용자보다 앞에 있는 대기열 수 확인
-        long currentPosition = queueRepository.countByStatusAndQueuePositionLessThan("WAITING", queue.getQueuePosition()) + 1;
-
-        long remainingTime = calculateRemainingTimeUseCase.calculateRemainingTime(queue.getExpiredAt());
-
-        return new QueueOutput(
-                queue.getUserId(),
-                queue.getToken(),
-                queue.getStatus(),
-                queue.getEnteredAt(),
-                queue.getExpiredAt(),
-                queue.getQueuePosition(),
-                currentPosition,
-                activeCount,
-                remainingTime
-        );
+        // Redis의 rank는 0부터 시작하므로 1을 더해서 순서 반환
+        return rank + 1;
     }
 }
